24d46829b2b74d57484b33aed95a6e093efd0eb8
Bishwas Bhandari <bishwasbh@gmail.com>
2023-12-03 19:46:31 +0545

    body_short_escaped added

--- posts/schemas.py
+++ posts/schemas.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from django.utils.html import strip_tags, strip_spaces_between_tags
 from ninja import Schema
 import posts.models
 from authentication.schemas.user_schema import GenericUserSchema
@@ -43,10 +44,15 @@ class PostSchema(GenericSchema):
     publishable: PublishableSchema
     author: GenericUserSchema
     tags: list[TagsSchema]
+    body_short_escaped: str
 
     @staticmethod
     def resolve_tags(obj: 'posts.models.Post'):
         return obj.tags.all()
+
+    @staticmethod
+    def resolve_body_short_escaped(obj: 'posts.models.Post'):
+        return strip_tags(obj.body)[:200] + '...' if len(obj.body) > 200 else strip_tags(obj.body)
 
     @staticmethod
     def resolve_publishable(obj: 'posts.models.Post'):
@@ -69,5 +75,3 @@ class CommentSchema(GenericSchema):
 
 class CreateCommentSchema(Schema):
     comment_text: str
-
-
